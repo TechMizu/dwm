0ebc73dcd0f55669c19b8b9ca539a712f21e9504
Mizu <5218011+TechMizu@users.noreply.github.com>
2020-04-04 15:43:26 -0700

    new look

--- config.def.h
+++ config.def.h
@@ -1,23 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 13;       /* gap pixel between windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "hack:size=14" };
+static const char *fonts[]          = { "hack:size=12" };
 static const char dmenufont[]       = "hack:size=12";
-static const char col_gray1[]       = "#2c1029";
-static const char col_gray2[]       = "#c82842";
-static const char col_gray3[]       = "#f6dccb";
-static const char col_gray4[]       = "#f3ba8d";
-static const char col_cyan[]        = "#f3815d";
+static const char col_gray1[]       = "#d7cbaa";
+static const char col_gray2[]       = "#766b57";
+static const char col_gray3[]       = "#484848";
+static const char col_gray4[]       = "#919191";
+static const char col_cyan[]        = "#d7cbaa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray3 },
 	[SchemeSel]  = { col_gray4, col_gray1, col_cyan },
 };
 
@@ -31,9 +31,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     iscentered     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,             1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,             0,           -1 },
-	{ "Code - OSS",NULL,	  NULL,	      1 << 2,       0,		   0,           -1 },
+	{ "Gimp",       NULL,     NULL,       0,            0,             1,           -1 },
+	{ "Firefox",    NULL,     NULL,       1 << 8,       0,             0,           -1 },
+	{ "Code - OSS", NULL,	  NULL,	      1 << 2,       0,		   0,           -1 },
 };
 
 /* layout(s) */
